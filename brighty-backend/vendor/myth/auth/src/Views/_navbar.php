d71083c982c6885f3f742806b1b7114e0417cf76
Tariq Abdullah <tariq@ideaquotient.in>
2022-05-04 22:35:03 +0600

    merged backedn and frontend

--- brighty-backend/vendor/myth/auth/src/Views/_navbar.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <img src="/logo.png" alt="" class="logo mx-auto d-block pb-5" style="max-width:200px">
-        </div>
-    </div>
-</div>
