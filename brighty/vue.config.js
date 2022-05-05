6ff26f911bdc7b292180d9e97a9f463c3b82dd03
Tariq Abdullah <tariq@ideaquotient.in>
2022-05-06 00:59:50 +0600

    Added webpack config to copy index.php from root to public on build

--- brighty/vue.config.js
+++ brighty/vue.config.js
@@ -1,6 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  outputDir: './public/',
+  configureWebpack: {
+    plugins: [
+      new CopyPlugin({
+        patterns: [
+          { from: "./index.php", to: "./index.php" },
+          { from: "./.htaccess", to: "./" }
+          
+        ],
+      }),
+    ]
+  }
+  
 })
